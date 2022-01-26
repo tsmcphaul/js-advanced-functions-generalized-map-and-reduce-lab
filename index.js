4a603a2707152fc3530d50516cd53fec06b4e972
Tammarra McPhaul <shaemc22@gmail.com>
2022-01-25 21:09:28 -0500

    commit tree c7283abc392b3e65b0cb6e78dbfe85d48b9b9e20

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(obj, fx) {
+    const arr = []
+    for (const key in obj) {
+      arr.push(fx(obj[key]))
+    }
+    return arr
+  }
+  
+  function reduce(obj, fx, acc) {
+    const keys = Object.keys(obj)
+    let i = 1
+  
+    if (acc) {
+      i = 0
+    } else {
+      acc = obj[keys[0]]
+    }
+  
+    for (i; i < keys.length; i++) {
+      acc = fx(acc, obj[keys[i]])
+    }
+    return acc
+  }
